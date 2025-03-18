a2d3f825bd80c2b29cf3df86775cadf4f5de0056
dan15098 <158103337+dan15098@users.noreply.github.com>
2025-03-18 15:53:16 +0000

    Update texttobinary.py

--- texttobinary.py
+++ texttobinary.py
@@ -1,1 +1,8 @@
+def toBinary(string):
+    binary = ''
+    for character in string:
+        binary = binary + bin(ord(character))[2:].zfill(8) + ' '
+    return binary.strip()
 
+
+print(toBinary(input('Enter text: ')))
